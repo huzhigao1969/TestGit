767354e442c29399a30ff1910e27823b096b8f99
huzhigao1969 <308207035@qq.com>
2019-05-01 22:53:59 +0800

    this is second commit

--- src/hello/HelloGit.java
+++ src/hello/HelloGit.java
@@ -1,8 +1,13 @@
 package hello;
+/*
 
+    this is V2.0
+
+ */
 public class HelloGit {
 
     public static void main(String[] args){
         System.out.println("Hello Git, I come from IDEA.");
+        System.out.println("Welcome ! Nice to meet you! ");
     }
 }
